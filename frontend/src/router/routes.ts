995334b128274212d51a8923d5ca28755f44315d
Renan Capuvilla <renan.capuvilla@claro.com.br>
2020-06-14 15:20:31 -0300

    Alterações na rota

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,28 +1,28 @@
-import { RouteConfig } from "vue-router"
+import { RouteConfig } from 'vue-router'
 
 const routes: RouteConfig[] = [
   {
-    path: "/",
-    component: () => import("layouts/MainLogin.vue"),
+    path: '/',
+    component: () => import('layouts/MainLogin.vue'),
     children: [
-      { path: "", component: () => import("pages/login/Login.vue") },
-      { path: "login", component: () => import("pages/login/Login.vue") },
-      { path: "register", component: () => import("pages/register/Register.vue") },
-      { path: "truck", component: () => import("pages/truck/Truck.vue") }
+      { path: '', component: () => import('pages/login/Login.vue') },
+      { path: 'login', component: () => import('pages/login/Login.vue') },
+      { path: 'register', component: () => import('pages/register/Register.vue') },
+      { path: 'truck', component: () => import('pages/truck/Truck.vue') }
     ]
   },
   {
-    path: "/mapa",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/Mapa.vue") }]
+    path: '/mapa',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Mapa.vue') }]
   }
 ]
 
 // Always leave this as last one
-if (process.env.MODE !== "ssr") {
+if (process.env.MODE !== 'ssr') {
   routes.push({
-    path: "*",
-    component: () => import("pages/Error404.vue")
+    path: '*',
+    component: () => import('pages/Error404.vue')
   })
 }
 
