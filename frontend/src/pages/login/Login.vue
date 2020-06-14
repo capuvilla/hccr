e0401ca681d8b977ca92682231905102095dc83a
Renan Capuvilla <renan.capuvilla@claro.com.br>
2020-06-13 21:32:32 -0300

    Alterações tela de login

--- frontend/src/pages/login/Login.vue
+++ frontend/src/pages/login/Login.vue
@@ -1,13 +1,27 @@
 <template>
-  <q-page class="bg-grey-3 text-white">
-    <q-img
-      class="absolute-bottom-left"
-      src="statics/img/fundo.jpg"
-      spinner-color="white"
-    />
+  <q-page class="bg-image text-white">
     <q-page class="flex flex-center">
       <form @submit.prevent.stop="onSubmit" @reset.prevent.stop="onReset" class="q-gutter-md">
         <img alt="Meu Trecho" src="statics/img/logo.png">
+        <div>
+          <q-input color="black" bg-color="white" outlined bottom-slots dense v-model="usuario" label="Login"></q-input>
+          <q-input color="black" bg-color="white" outlined dense v-model="password" label="Senha" :type="isPwd ? 'password' : 'text'">
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
+          <a dense color="primary" style="cursor: pointer" @click="cdsenha()">Esqueceu sua senha?</a>
+        </div>
+        <div>
+          <q-btn class="glossy full-width" rounded color="deep-orange" label="ENTRAR" type="submit"/>
+          <br>
+          <br>
+          <q-btn class="full-width text-red" rounded color="white" label="Faça seu cadastro" type="submit"/>
+        </div>
       </form>
     </q-page>
   </q-page>
@@ -15,7 +29,14 @@
 
 <script>
 export default {
-  // name: 'PageName',
+  name: 'PageLogin',
+  data () {
+    return {
+      password: '',
+      usuario: '',
+      isPwd: true
+    }
+  }
 }
 </script>
 
@@ -23,4 +44,22 @@ export default {
 ::-webkit-scrollbar {
   display: none;
 }
+</style>
+
+<style>
+  .bg-image{
+    position: relative;
+    background-color: #000;
+  }
+  .bg-image::before{
+    content: ' ';
+    display: block;
+    position: absolute;
+    opacity: 0.5;
+    background: url('/statics/img/fundo.jpg');
+    width: 100%;
+    height: 100%;
+    background-position: right bottom;
+    background-size: cover;
+  }
 </style>
